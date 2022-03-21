3d337ae28027b84d973d212cc6b5a0372dd3736f
Josh <jzieve@Joshs-iMac.local>
2022-03-21 12:59:22 -0400

    controoler models and api

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,70 @@
+// const { process_params } = require('express/lib/router');
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // the functions gwill go in here as methods
+
+    // GET all pizzas: GET /api/pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get one pizza bu id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // create Pizza: POST /api/pizzas
+    createPizza({ body }, res) {
+        Pizza.create(body)
+            .then(dbPizzaData => res.json(dbPizzaData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // Updating a pizza when we make a request to PUT /api/pizzas/:id
+    // Update pizza by id
+    updatePizza({ params, body }, res) {
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+    // delete a pizza from the database when we make a request to DELETE /api/pizzas/:id
+    // delete pizza
+    deletePizza({ params }, res) {
+        Pizza.findOneAndDelete({ _id: params.id })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+
+}
+
+module.exports = pizzaController;

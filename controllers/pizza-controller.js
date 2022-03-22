f0fa9cea92bf2e13d14a360e749a3974dde0dd0d
Josh <jzieve@Joshs-iMac.local>
2022-03-22 17:38:30 -0400

    update

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -54,7 +54,7 @@ const pizzaController = {
     // Updating a pizza when we make a request to PUT /api/pizzas/:id
     // Update pizza by id
     updatePizza({ params, body }, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id' });
